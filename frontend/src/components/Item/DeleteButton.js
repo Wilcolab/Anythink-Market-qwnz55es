c15a925a9334c241b7b4d1e0130de7a89a27319d
Hophy1 <112934313+Hophy1@users.noreply.github.com>
2023-01-23 13:32:08 +0000

    lint

--- frontend/src/components/Item/DeleteButton.js
+++ frontend/src/components/Item/DeleteButton.js
@@ -5,9 +5,9 @@ import { DELETE_COMMENT } from "../../constants/actionTypes";
 
 const mapDispatchToProps = (dispatch) => ({
   onClick: (payload, commentId) =>
-    dispatch({ 
-      type: DELETE_COMMENT, 
-      payload, commentId 
+    dispatch({
+      type: DELETE_COMMENT,
+      payload, commentId
     }),
 });
 
